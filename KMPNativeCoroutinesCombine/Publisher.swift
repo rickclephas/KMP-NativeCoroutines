a4793df26fbf7475a6e8c8f43c56044659154fd7
Rick Clephas <rclephas@gmail.com>
2021-06-12 22:35:34 +0200

    Create custom Combine publishers to fix issue with dropped values

--- KMPNativeCoroutinesCombine/Publisher.swift
+++ KMPNativeCoroutinesCombine/Publisher.swift
@@ -14,21 +14,48 @@ import KMPNativeCoroutinesCore
 public func createPublisher<Output, Failure: Error, Unit>(
     for nativeFlow: @escaping NativeFlow<Output, Failure, Unit>
 ) -> AnyPublisher<Output, Failure> {
-    return Deferred<Publishers.HandleEvents<PassthroughSubject<Output, Failure>>> {
-        let subject = PassthroughSubject<Output, Failure>()
-        let cancel = nativeFlow({ item, unit in
-            subject.send(item)
+    return NativeFlowPublisher(nativeFlow: nativeFlow)
+        .eraseToAnyPublisher()
+}
+
+internal struct NativeFlowPublisher<Output, Failure: Error, Unit>: Publisher {
+    
+    typealias Output = Output
+    typealias Failure = Failure
+    
+    let nativeFlow: NativeFlow<Output, Failure, Unit>
+    
+    func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        let subscription = NativeFlowSubscription(nativeFlow: nativeFlow, subscriber: subscriber)
+        subscriber.receive(subscription: subscription)
+    }
+}
+
+internal class NativeFlowSubscription<Output, Failure, Unit, S: Subscriber>: Subscription where S.Input == Output, S.Failure == Failure {
+    
+    private var nativeCancellable: NativeCancellable<Unit>? = nil
+    private var subscriber: S?
+    
+    init(nativeFlow: NativeFlow<Output, Failure, Unit>, subscriber: S) {
+        self.subscriber = subscriber
+        nativeCancellable = nativeFlow({ item, unit in
+            _ = self.subscriber?.receive(item)
             return unit
         }, { error, unit in
             if let error = error {
-                subject.send(completion: .failure(error))
+                self.subscriber?.receive(completion: .failure(error))
             } else {
-                subject.send(completion: .finished)
+                self.subscriber?.receive(completion: .finished)
             }
             return unit
         })
-        return subject.handleEvents(receiveCancel: {
-            _ = cancel()
-        })
-    }.eraseToAnyPublisher()
+    }
+    
+    func request(_ demand: Subscribers.Demand) { }
+    
+    func cancel() {
+        subscriber = nil
+        _ = nativeCancellable?()
+        nativeCancellable = nil
+    }
 }
