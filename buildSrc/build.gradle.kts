cc8e1c6199ef382ac8d9d79ecbc55097dc5b0b8b
Rick Clephas <rclephas@gmail.com>
2021-07-03 14:07:13 +0200

    Move publishing logic to buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 repositories {
-    mavenCentral()
+    gradlePluginPortal()
 }
---
+++ buildSrc/src/main/kotlin/kmp-nativecoroutines-publish.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    `maven-publish`
+    signing
+}
+
+ext["signing.keyId"] = null
+ext["signing.password"] = null
+ext["signing.secretKeyRingFile"] = null
+ext["ossrhUsername"] = null
+ext["ossrhPassword"] = null
+val localPropsFile = project.rootProject.file("local.properties")
+if (localPropsFile.exists()) {
+    localPropsFile.reader()
+        .use { java.util.Properties().apply { load(it) } }
+        .onEach { (name, value) -> ext[name.toString()] = value }
+} else {
+    ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+    ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
+    ext["signing.secretKeyRingFile"] = System.getenv("SIGNING_SECRET_KEY_RING_FILE")
+    ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+    ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+}
+
+val emptyJavadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+fun getExtraString(name: String) = ext[name]?.toString()
+
+publishing {
+    repositories {
+        maven {
+            name = "sonatype"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+        }
+    }
+
+    publications.withType<MavenPublication> {
+        artifact(emptyJavadocJar.get())
+
+        pom {
+            name.set("KMP-NativeCoroutines")
+            description.set("Swift library for Kotlin Coroutines")
+            url.set("https://github.com/rickclephas/KMP-NativeCoroutines")
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set("rickclephas")
+                    name.set("Rick Clephas")
+                    email.set("rclephas@gmail.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/rickclephas/KMP-NativeCoroutines")
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
+}
