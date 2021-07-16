03890f8a294c552910630e8fb785d8e037df9b9e
Rick Clephas <rclephas@gmail.com>
2021-07-16 21:59:57 +0200

    Add asyncStream integration tests

---
+++ sample/Async/AsyncStreamIntegrationTests.swift
@@ -0,0 +1,162 @@
+//
+//  AsyncStreamIntegrationTests.swift
+//  AsyncStreamIntegrationTests
+//
+//  Created by Rick Clephas on 16/07/2021.
+//
+
+import XCTest
+import KMPNativeCoroutinesAsync
+import NativeCoroutinesSampleShared
+
+class AsyncStreamIntegrationTests: XCTestCase {
+    
+    override func setUp() {
+        CoroutinesAppleKt.doInitCoroutinesFromMainThread()
+    }
+    
+    func testValuesReceived() async {
+        let integrationTests = FlowIntegrationTests()
+        let sendValueCount = randomInt(min: 5, max: 20)
+        let stream = asyncStream(for: integrationTests.getFlowNative(count: sendValueCount, delay: 100))
+        do {
+            var receivedValueCount: Int32 = 0
+            for try await value in stream {
+                XCTAssertEqual(integrationTests.uncompletedJobCount, 1, "There should be 1 uncompleted job")
+                XCTAssertEqual(value.int32Value, receivedValueCount, "Received incorrect value")
+                receivedValueCount += 1
+            }
+            XCTAssertEqual(receivedValueCount, sendValueCount, "Should have received all values")
+        } catch {
+            XCTFail("Stream should complete without an error")
+        }
+        await withCheckedContinuation { (continuation: CheckedContinuation<Void, Never>) in
+            // The job should complete soon after the stream finishes
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
+                XCTAssertEqual(integrationTests.uncompletedJobCount, 0, "The job should have completed by now")
+                continuation.resume()
+            }
+        }
+    }
+    
+    func testNilValueReceived() async {
+        let integrationTests = FlowIntegrationTests()
+        let sendValueCount = randomInt(min: 5, max: 20)
+        let nullValueIndex = randomInt(min: 0, max: sendValueCount - 1)
+        let stream = asyncStream(for: integrationTests.getFlowWithNullNative(count: sendValueCount, nullIndex: nullValueIndex, delay: 100))
+        do {
+            var receivedValueCount: Int32 = 0
+            for try await value in stream {
+                XCTAssertEqual(integrationTests.uncompletedJobCount, 1, "There should be 1 uncompleted job")
+                if receivedValueCount == nullValueIndex {
+                    XCTAssertNil(value, "Value should be nil")
+                } else {
+                    XCTAssertEqual(value?.int32Value, receivedValueCount, "Received incorrect value")
+                }
+                receivedValueCount += 1
+            }
+            XCTAssertEqual(receivedValueCount, sendValueCount, "Should have received all values")
+        } catch {
+            XCTFail("Stream should complete without an error")
+        }
+        await withCheckedContinuation { (continuation: CheckedContinuation<Void, Never>) in
+            // The job should complete soon after the stream finishes
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
+                XCTAssertEqual(integrationTests.uncompletedJobCount, 0, "The job should have completed by now")
+                continuation.resume()
+            }
+        }
+    }
+    
+    func testExceptionReceived() async {
+        let integrationTests = FlowIntegrationTests()
+        let sendValueCount = randomInt(min: 5, max: 20)
+        let exceptionIndex = randomInt(min: 1, max: sendValueCount - 1)
+        let sendMessage = randomString()
+        let stream = asyncStream(for: integrationTests.getFlowWithExceptionNative(count: sendValueCount, exceptionIndex: exceptionIndex, message: sendMessage, delay: 100))
+        var receivedValueCount: Int32 = 0
+        do {
+            for try await _ in stream {
+                XCTAssertEqual(integrationTests.uncompletedJobCount, 1, "There should be 1 uncompleted job")
+                receivedValueCount += 1
+            }
+            XCTFail("Stream should fail with an error")
+        } catch {
+            let error = error as NSError
+            XCTAssertEqual(error.localizedDescription, sendMessage, "Error has incorrect localizedDescription")
+            let exception = error.userInfo["KotlinException"]
+            XCTAssertTrue(exception is KotlinException, "Error doesn't contain the Kotlin exception")
+        }
+        await withCheckedContinuation { (continuation: CheckedContinuation<Void, Never>) in
+            // The job should complete soon after the stream finishes
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
+                XCTAssertEqual(integrationTests.uncompletedJobCount, 0, "The job should have completed by now")
+                continuation.resume()
+            }
+        }
+        XCTAssertEqual(receivedValueCount, exceptionIndex, "Should have received all values before the exception")
+    }
+    
+    func testErrorReceived() async {
+        let integrationTests = FlowIntegrationTests()
+        let sendValueCount = randomInt(min: 5, max: 20)
+        let errorIndex = randomInt(min: 1, max: sendValueCount - 1)
+        let sendMessage = randomString()
+        let stream = asyncStream(for: integrationTests.getFlowWithErrorNative(count: sendValueCount, errorIndex: errorIndex, message: sendMessage, delay: 100))
+        var receivedValueCount: Int32 = 0
+        do {
+            for try await _ in stream {
+                XCTAssertEqual(integrationTests.uncompletedJobCount, 1, "There should be 1 uncompleted job")
+                receivedValueCount += 1
+            }
+            XCTFail("Stream should fail with an error")
+        } catch {
+            let error = error as NSError
+            XCTAssertEqual(error.localizedDescription, sendMessage, "Error has incorrect localizedDescription")
+            let exception = error.userInfo["KotlinException"]
+            XCTAssertTrue(exception is KotlinThrowable, "Error doesn't contain the Kotlin error")
+        }
+        await withCheckedContinuation { (continuation: CheckedContinuation<Void, Never>) in
+            // The job should complete soon after the stream finishes
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
+                XCTAssertEqual(integrationTests.uncompletedJobCount, 0, "The job should have completed by now")
+                continuation.resume()
+            }
+        }
+        XCTAssertEqual(receivedValueCount, errorIndex, "Should have received all values before the error")
+    }
+    
+    func testCancellation() async {
+        let integrationTests = FlowIntegrationTests()
+        var callbackCount = 0
+        let stream = asyncStream(for: integrationTests.getFlowWithCallbackNative(count: 5, callbackIndex: 2, delay: 1000) {
+            callbackCount += 1
+        })
+        let handle = Task<Int32, Never> {
+            do {
+                var receivedValueCount: Int32 = 0
+                for try await _ in stream {
+                    XCTAssertEqual(integrationTests.uncompletedJobCount, 1, "There should be 1 uncompleted job")
+                    receivedValueCount += 1
+                }
+                return receivedValueCount
+            } catch {
+                XCTFail("Stream should be cancelled without an error")
+                return -1
+            }
+        }
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+            XCTAssertEqual(integrationTests.activeJobCount, 1, "There should be 1 active job")
+            handle.cancel()
+        }
+        _ = await handle.result
+        await withCheckedContinuation { (continuation: CheckedContinuation<Void, Never>) in
+            // The job should complete soon after the stream finishes
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
+                XCTAssertEqual(integrationTests.uncompletedJobCount, 0, "The job should have completed by now")
+                continuation.resume()
+            }
+        }
+        XCTAssertEqual(callbackCount, 0, "The callback shouldn't be called")
+    }
+}
