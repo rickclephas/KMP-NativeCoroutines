0028b47b67e9b03b5eb4b18549f4f0e28badcf62
Rick Clephas <rclephas@gmail.com>
2021-07-19 19:42:35 +0200

    Explicitly specify return types

--- sample/shared/src/commonMain/kotlin/com/rickclephas/kmp/nativecoroutines/sample/Logger.kt
+++ sample/shared/src/commonMain/kotlin/com/rickclephas/kmp/nativecoroutines/sample/Logger.kt
@@ -9,21 +9,21 @@ abstract class Logger {
 
     abstract fun log(severity: Severity, message: String, tag: String, throwable: Throwable? = null)
 
-    open fun v(message: String, tag: String, throwable: Throwable? = null) =
+    open fun v(message: String, tag: String, throwable: Throwable? = null): Unit =
         log(Severity.Verbose, message, tag, throwable)
 
-    open fun d(message: String, tag: String, throwable: Throwable? = null) =
+    open fun d(message: String, tag: String, throwable: Throwable? = null): Unit =
         log(Severity.Debug, message, tag, throwable)
 
-    open fun i(message: String, tag: String, throwable: Throwable? = null) =
+    open fun i(message: String, tag: String, throwable: Throwable? = null): Unit =
         log(Severity.Info, message, tag, throwable)
 
-    open fun w(message: String, tag: String, throwable: Throwable? = null) =
+    open fun w(message: String, tag: String, throwable: Throwable? = null): Unit =
         log(Severity.Warn, message, tag, throwable)
 
-    open fun e(message: String, tag: String, throwable: Throwable? = null) =
+    open fun e(message: String, tag: String, throwable: Throwable? = null): Unit =
         log(Severity.Error, message, tag, throwable)
 
-    open fun wtf(message: String, tag: String, throwable: Throwable? = null) =
+    open fun wtf(message: String, tag: String, throwable: Throwable? = null): Unit =
         log(Severity.Assert, message, tag, throwable)
 }
