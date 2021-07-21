af78d01433f98144e461b794f60b291354b93659
Rick Clephas <rclephas@gmail.com>
2021-07-21 21:48:35 +0200

    Add Kotlin version suffix to version

--- sample/shared/build.gradle.kts
+++ sample/shared/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
-    id("com.rickclephas.kmp.nativecoroutines") version "0.4.2"
+    id("com.rickclephas.kmp.nativecoroutines") version "0.4.2-kotlin-1.5.10"
 }
 
 kotlin {
