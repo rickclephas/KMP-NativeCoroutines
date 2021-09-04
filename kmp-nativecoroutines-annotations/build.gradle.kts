9daa071cde92e0be06de9d0612062b8d3097e021
Rick Clephas <rclephas@gmail.com>
2021-09-04 18:05:14 +0200

    Add tvOS and watchOS targets

--- kmp-nativecoroutines-annotations/build.gradle.kts
+++ kmp-nativecoroutines-annotations/build.gradle.kts
@@ -8,4 +8,9 @@ kotlin {
     macosX64()
     iosArm64()
     iosX64()
+    watchosArm32()
+    watchosArm64()
+    watchosX64()
+    tvosArm64()
+    tvosX64()
 }
--- kmp-nativecoroutines-core/build.gradle.kts
+++ kmp-nativecoroutines-core/build.gradle.kts
@@ -5,9 +5,14 @@ plugins {
 
 kotlin {
     jvm()
-    macosX64()
-    iosArm64()
-    iosX64()
+    val macosX64 = macosX64()
+    val iosArm64 = iosArm64()
+    val iosX64 = iosX64()
+    val watchosArm32 = watchosArm32()
+    val watchosArm64 = watchosArm64()
+    val watchosX64 = watchosX64()
+    val tvosArm64 = tvosArm64()
+    val tvosX64 = tvosX64()
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -25,23 +30,18 @@ kotlin {
         val appleTest by creating {
             dependsOn(commonTest)
         }
-        val macosX64Main by getting {
-            dependsOn(appleMain)
-        }
-        val macosX64Test by getting {
-            dependsOn(appleTest)
-        }
-        val iosArm64Main by getting {
-            dependsOn(appleMain)
-        }
-        val iosArm64Test by getting {
-            dependsOn(appleTest)
-        }
-        val iosX64Main by getting {
-            dependsOn(appleMain)
-        }
-        val iosX64Test by getting {
-            dependsOn(appleTest)
+        listOf(
+            macosX64,
+            iosArm64, iosX64,
+            watchosArm32, watchosArm64, watchosX64,
+            tvosArm64, tvosX64
+        ).forEach {
+            getByName("${it.targetName}Main") {
+                dependsOn(appleMain)
+            }
+            getByName("${it.targetName}Test") {
+                dependsOn(appleTest)
+            }
         }
     }
 }
