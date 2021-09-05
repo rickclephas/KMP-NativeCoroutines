5ca4615b70eb680088726a5d433a185ea719b0cf
Rick Clephas <rclephas@gmail.com>
2021-09-05 17:13:45 +0200

    Update sample

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,7 +8,6 @@ object Dependencies {
 
     object Kotlinx {
         const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt"
-        const val datetime = "org.jetbrains.kotlinx:kotlinx-datetime:0.2.1"
     }
 
     object AutoService {
