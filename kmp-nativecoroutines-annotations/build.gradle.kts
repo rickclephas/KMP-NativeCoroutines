12a8de343c4bf3416a6496f37d9da69c04b9cb14
Rick Clephas <rclephas@gmail.com>
2021-09-05 16:23:48 +0200

    Add Apple Silicon targets #9

--- kmp-nativecoroutines-annotations/build.gradle.kts
+++ kmp-nativecoroutines-annotations/build.gradle.kts
@@ -6,11 +6,15 @@ plugins {
 kotlin {
     jvm()
     macosX64()
+    macosArm64()
     iosArm64()
     iosX64()
+    iosSimulatorArm64()
     watchosArm32()
     watchosArm64()
     watchosX64()
+    watchosSimulatorArm64()
     tvosArm64()
     tvosX64()
+    tvosSimulatorArm64()
 }
--- kmp-nativecoroutines-core/build.gradle.kts
+++ kmp-nativecoroutines-core/build.gradle.kts
@@ -6,13 +6,17 @@ plugins {
 kotlin {
     jvm()
     val macosX64 = macosX64()
+    val macosArm64 = macosArm64()
     val iosArm64 = iosArm64()
     val iosX64 = iosX64()
+    val iosSimulatorArm64 = iosSimulatorArm64()
     val watchosArm32 = watchosArm32()
     val watchosArm64 = watchosArm64()
     val watchosX64 = watchosX64()
+    val watchosSimulatorArm64 = watchosSimulatorArm64()
     val tvosArm64 = tvosArm64()
     val tvosX64 = tvosX64()
+    val tvosSimulatorArm64 = tvosSimulatorArm64()
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -31,10 +35,10 @@ kotlin {
             dependsOn(commonTest)
         }
         listOf(
-            macosX64,
-            iosArm64, iosX64,
-            watchosArm32, watchosArm64, watchosX64,
-            tvosArm64, tvosX64
+            macosX64, macosArm64,
+            iosArm64, iosX64, iosSimulatorArm64,
+            watchosArm32, watchosArm64, watchosX64, watchosSimulatorArm64,
+            tvosArm64, tvosX64, tvosSimulatorArm64
         ).forEach {
             getByName("${it.targetName}Main") {
                 dependsOn(appleMain)
