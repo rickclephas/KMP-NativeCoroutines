11977f0a82f2481a9e7095556f72f027c549f55b
Rick Clephas <rclephas@gmail.com>
2021-11-02 20:35:18 +0100

    Downgrade supported platforms

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "KMPNativeCoroutines",
-    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+    platforms: [.iOS(.v9), .macOS(.v10_10), .tvOS(.3), .watchOS(.v9)],
     products: [
         .library(
             name: "KMPNativeCoroutinesCore",
