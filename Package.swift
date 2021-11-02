914e72d987ce1b3dc8244afc79d1f64d6ee210a1
Rick Clephas <rclephas@gmail.com>
2021-11-02 20:27:01 +0100

    Remove SPM products/targets/dependencies

--- Package.swift
+++ Package.swift
@@ -10,38 +10,14 @@ let package = Package(
             targets: ["KMPNativeCoroutinesCore"]
         ),
         .library(
-            name: "KMPNativeCoroutinesCombine",
-            targets: ["KMPNativeCoroutinesCombine"]
-        ),
-        .library(
             name: "KMPNativeCoroutinesAsync",
             targets: ["KMPNativeCoroutinesAsync"]
-        ),
-        .library(
-            name: "KMPNativeCoroutinesRxSwift",
-            targets: ["KMPNativeCoroutinesRxSwift"]
-        )
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/ReactiveX/RxSwift.git",
-            from: "6.0.0"
         )
     ],
     targets: [
         .target(
             name: "KMPNativeCoroutinesCore",
             path: "KMPNativeCoroutinesCore"
-        ),
-        .target(
-            name: "KMPNativeCoroutinesCombine",
-            dependencies: ["KMPNativeCoroutinesCore"],
-            path: "KMPNativeCoroutinesCombine"
-        ),
-        .testTarget(
-            name: "KMPNativeCoroutinesCombineTests",
-            dependencies: ["KMPNativeCoroutinesCombine"],
-            path: "KMPNativeCoroutinesCombineTests"
         ),
         .target(
             name: "KMPNativeCoroutinesAsync",
@@ -52,16 +28,6 @@ let package = Package(
             name: "KMPNativeCoroutinesAsyncTests",
             dependencies: ["KMPNativeCoroutinesAsync"],
             path: "KMPNativeCoroutinesAsyncTests"
-        ),
-        .target(
-            name: "KMPNativeCoroutinesRxSwift",
-            dependencies: ["KMPNativeCoroutinesCore", "RxSwift"],
-            path: "KMPNativeCoroutinesRxSwift"
-        ),
-        .testTarget(
-            name: "KMPNativeCoroutinesRxSwiftTests",
-            dependencies: ["KMPNativeCoroutinesRxSwift"],
-            path: "KMPNativeCoroutinesRxSwiftTests"
         )
     ],
     swiftLanguageVersions: [.v5]
