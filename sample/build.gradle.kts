2608da9d088b657a14b4c4419964381722269fa1
Rick Clephas <rclephas@gmail.com>
2021-11-05 14:02:34 +0100

    Downgrade sample Kotlin version

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
         classpath("com.rickclephas.kmp:kmp-nativecoroutines-gradle-plugin")
     }
 }
