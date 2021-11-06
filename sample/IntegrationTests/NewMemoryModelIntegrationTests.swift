1a1a12839432fd318b76d06fb370ab80ccda3099
Rick Clephas <rclephas@gmail.com>
2021-11-06 19:36:21 +0100

    Add integration tests for the new memory model

---
+++ sample/IntegrationTests/NewMemoryModelIntegrationTests.swift
@@ -0,0 +1,34 @@
+//
+//  NewMemoryModelIntegrationTests.swift
+//  Sample
+//
+//  Created by Rick Clephas on 06/11/2021.
+//
+
+import XCTest
+import KMPNativeCoroutinesCore
+import NativeCoroutinesSampleShared
+
+class NewMemoryModelIntegrationTests: XCTestCase {
+    
+    private typealias IntegrationTests = NativeCoroutinesSampleShared.NewMemoryModelIntegrationTests
+    
+    func testReturnMutableData() {
+        let integrationTests = IntegrationTests()
+        let valueExpectation = expectation(description: "Waiting for value")
+        _ = integrationTests.generateRandomMutableDataNative()({ value, unit in
+            let dataFromBackground = value.dataFromBackground
+            let dataFromMain = value.dataFromMain
+            XCTAssertNotNil(dataFromBackground, "Data from background should be set")
+            XCTAssertNotNil(dataFromMain, "Data from main should be set")
+            XCTAssertNotEqual(dataFromBackground, dataFromMain, "Data from background and main should be different")
+            value.dataFromBackground = dataFromMain
+            value.dataFromMain = dataFromBackground
+            XCTAssertEqual(value.dataFromBackground, dataFromMain, "Data from background should now be data from main")
+            XCTAssertEqual(value.dataFromMain, dataFromBackground, "Data from background should now be data from main")
+            valueExpectation.fulfill()
+            return unit
+        }, { _, unit in unit })
+        wait(for: [valueExpectation], timeout: 1)
+    }
+}
---
+++ sample/shared/src/commonMain/kotlin/com/rickclephas/kmp/nativecoroutines/sample/NewMemoryModelIntegrationTests.kt
@@ -0,0 +1,32 @@
+package com.rickclephas.kmp.nativecoroutines.sample
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlin.random.Random
+
+class NewMemoryModelIntegrationTests: IntegrationTests() {
+
+    class MutableData {
+        var dataFromBackground: String? = null
+        var dataFromMain: String? = null
+    }
+
+    private fun randomString(): String {
+        val chars = mutableListOf<Char>()
+        repeat(5) {
+            chars.add(Random.nextInt(65, 91).toChar())
+        }
+        return chars.joinToString("")
+    }
+
+    suspend fun generateRandomMutableData(): MutableData {
+        val data = MutableData()
+        withContext(Dispatchers.Main) {
+            data.dataFromMain = randomString()
+            withContext(Dispatchers.Default) {
+                data.dataFromBackground = randomString()
+            }
+        }
+        return data
+    }
+}
