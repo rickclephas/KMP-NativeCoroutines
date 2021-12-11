c352ba090f061e12433a11498add3318ad42194c
Rick Clephas <rclephas@gmail.com>
2021-12-11 13:03:25 +0100

    OptIn to internal coroutines API
    https://github.com/Kotlin/kotlinx.coroutines/issues/3078

--- kmp-nativecoroutines-core/src/appleMain/kotlin/com/rickclephas/kmp/nativecoroutines/NativeFlow.kt
+++ kmp-nativecoroutines-core/src/appleMain/kotlin/com/rickclephas/kmp/nativecoroutines/NativeFlow.kt
@@ -2,6 +2,7 @@ package com.rickclephas.kmp.nativecoroutines
 
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -22,6 +23,7 @@ typealias NativeFlow<T> = (onItem: NativeCallback<T>, onComplete: NativeCallback
  * @receiver the [Flow] to collect.
  * @see Flow.collect
  */
+@OptIn(InternalCoroutinesApi::class)
 fun <T> Flow<T>.asNativeFlow(scope: CoroutineScope? = null): NativeFlow<T> {
     val coroutineScope = scope ?: defaultCoroutineScope
     return (collect@{ onItem: NativeCallback<T>, onComplete: NativeCallback<NSError?> ->
