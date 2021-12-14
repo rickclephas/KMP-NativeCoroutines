b93e5ef98cda11da175c6ab0f199bcc414ca2380
Rick Clephas <rclephas@gmail.com>
2021-12-14 21:33:32 +0100

    Bump Kotlin version to 1.6.10

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10-RC")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.rickclephas.kmp:kmp-nativecoroutines-gradle-plugin")
     }
 }
