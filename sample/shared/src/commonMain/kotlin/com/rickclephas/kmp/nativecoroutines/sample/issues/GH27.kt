0ccbec0503e7f01d76f39ea29acbf7e25bb45408
Rick Clephas <rclephas@gmail.com>
2021-12-19 13:35:54 +0100

    Minimize reproduction sample

---
+++ sample/shared/src/commonMain/kotlin/com/rickclephas/kmp/nativecoroutines/sample/issues/GH27.kt
@@ -0,0 +1,33 @@
+package com.rickclephas.kmp.nativecoroutines.sample.issues
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+/**
+ * This class will make the compiler fail with a recursion error
+ * when both KMP-NativeCoroutines and `kotlinx.serialization` are applied to the project.
+ * The recursion is triggered by the [descriptor] property,
+ * to work around this error the return type of [descriptor] can be specified explicitly:
+ * ```
+ * override val descriptor: SerialDescriptor = ...
+ * ```
+ */
+@Suppress("unused")
+@OptIn(ExperimentalSerializationApi::class)
+@Serializer(ByteArray::class)
+object ByteArraySerializer : KSerializer<ByteArray> {
+
+    override val descriptor =
+        PrimitiveSerialDescriptor(ByteArraySerializer::class.qualifiedName!!, PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: ByteArray) =
+        encoder.encodeString(value.decodeToString())
+
+    override fun deserialize(decoder: Decoder) =
+       decoder.decodeString().encodeToByteArray()
+}
