0fd6b825bd464bb1b2123a54a552e6244d8ffa91
Ankush Gupta <me@ankushg.com>
2022-02-16 15:02:25 -0800

    create intermediate supportedTargetMain and supportedTargetTest sourcesets
    goes between common and js/apple

--- kmp-nativecoroutines-core/build.gradle.kts
+++ kmp-nativecoroutines-core/build.gradle.kts
@@ -37,11 +37,17 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
-        val appleMain by creating {
+        val supportedTargetMain by creating {
             dependsOn(commonMain)
         }
-        val appleTest by creating {
+        val supportedTargetTest by creating {
             dependsOn(commonTest)
+        }
+        val appleMain by creating {
+            dependsOn(supportedTargetMain)
+        }
+        val appleTest by creating {
+            dependsOn(supportedTargetTest)
         }
         listOf(
             macosX64, macosArm64,
@@ -55,6 +61,12 @@ kotlin {
             getByName("${it.targetName}Test") {
                 dependsOn(appleTest)
             }
+        }
+        val jsMain by getting {
+            dependsOn(supportedTargetMain)
+        }
+        val jsTest by getting {
+            dependsOn(supportedTargetTest)
         }
     }
 }
