f0038b840f348663086aa62148610aa1f14ab249
Rick Clephas <rclephas@gmail.com>
2022-03-26 11:36:43 +0100

    Update Kotlin to 1.6.20-RC2

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20-RC")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.20-RC")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20-RC2")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.20-RC2")
         classpath("com.rickclephas.kmp:kmp-nativecoroutines-gradle-plugin")
     }
 }
