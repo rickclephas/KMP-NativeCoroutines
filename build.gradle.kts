352512e0ca3f34181eca3cd52ee433b95b7c0697
Rick Clephas <rclephas@gmail.com>
2022-05-20 19:43:49 +0200

    Update Kotlin version suffix

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 
 allprojects {
     group = "com.rickclephas.kmp"
-    version = "0.12.2-kotlin-1.7.0-Beta"
+    version = "0.12.2-kotlin-1.7.0-RC"
 
     repositories {
         mavenCentral()
