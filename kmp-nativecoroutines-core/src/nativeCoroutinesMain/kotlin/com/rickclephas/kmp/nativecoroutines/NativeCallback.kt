f3caca202901a9d849aabf9a3260feb6d1d8beb2
Rick Clephas <rclephas@gmail.com>
2022-08-06 12:05:26 +0200

    Remove freeze calls

--- kmp-nativecoroutines-core/src/nativeCoroutinesMain/kotlin/com/rickclephas/kmp/nativecoroutines/NativeCallback.kt
+++ kmp-nativecoroutines-core/src/nativeCoroutinesMain/kotlin/com/rickclephas/kmp/nativecoroutines/NativeCallback.kt
@@ -3,7 +3,7 @@ package com.rickclephas.kmp.nativecoroutines
 /**
  * A callback with a single argument.
  *
- * We don't want the Swift code to known how to get the [Unit] object, so we'll provide it as the second argument.
+ * We don't want the Swift code to know how to get the [Unit] object, so we'll provide it as the second argument.
  * This way Swift can just return the value that it received without knowing what it is/how to get it.
  */
 typealias NativeCallback<T> = (T, Unit) -> Unit
@@ -12,12 +12,12 @@ typealias NativeCallback<T> = (T, Unit) -> Unit
  * Invokes the callback with the specified [value].
  */
 internal inline operator fun <T> NativeCallback<T>.invoke(value: T) =
-    invoke(value.freeze(), Unit)
+    invoke(value, Unit)
 
 /**
  * A callback with two arguments.
  *
- * We don't want the Swift code to known how to get the [Unit] object, so we'll provide it as the third argument.
+ * We don't want the Swift code to know how to get the [Unit] object, so we'll provide it as the third argument.
  * This way Swift can just return the value that it received without knowing what it is/how to get it.
  */
 typealias NativeCallback2<T1, T2> = (T1, T2, Unit) -> Unit
@@ -26,4 +26,4 @@ typealias NativeCallback2<T1, T2> = (T1, T2, Unit) -> Unit
  * Invokes the callback with the specified [value1] and [value2].
  */
 internal inline operator fun <T1, T2> NativeCallback2<T1, T2>.invoke(value1: T1, value2: T2) =
-    invoke(value1.freeze(), value2.freeze(), Unit)
+    invoke(value1, value2, Unit)
