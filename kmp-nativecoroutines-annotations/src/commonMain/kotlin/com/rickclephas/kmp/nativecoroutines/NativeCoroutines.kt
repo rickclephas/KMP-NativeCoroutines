37f2e32faea7b5eefe4d6ad25fc83cedcc3ba1b4
Rick Clephas <rclephas@gmail.com>
2022-08-07 20:14:42 +0200

    Add NativeCoroutines annotation

---
+++ kmp-nativecoroutines-annotations/src/commonMain/kotlin/com/rickclephas/kmp/nativecoroutines/NativeCoroutines.kt
@@ -0,0 +1,6 @@
+package com.rickclephas.kmp.nativecoroutines
+
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class NativeCoroutines
