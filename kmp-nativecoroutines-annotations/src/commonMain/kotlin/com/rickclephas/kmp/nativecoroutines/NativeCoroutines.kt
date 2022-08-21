a2e6aead404a08bdcfee83f6035f41723b4165ac
Rick Clephas <rclephas@gmail.com>
2022-08-21 14:59:47 +0200

    Add KDoc

--- kmp-nativecoroutines-annotations/src/commonMain/kotlin/com/rickclephas/kmp/nativecoroutines/NativeCoroutines.kt
+++ kmp-nativecoroutines-annotations/src/commonMain/kotlin/com/rickclephas/kmp/nativecoroutines/NativeCoroutines.kt
@@ -1,5 +1,8 @@
 package com.rickclephas.kmp.nativecoroutines
 
+/**
+ * Identifies properties and functions that require a native coroutines version.
+ */
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.SOURCE)
 @MustBeDocumented
