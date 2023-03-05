c8b2ac50253f69960f5e15daa71ac14e94d866ae
Rick Clephas <rclephas@gmail.com>
2023-03-05 22:24:05 +0100

    Add Kotlin version suffix

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 
 allprojects {
     group = "com.rickclephas.kmp"
-    version = "1.0.0-ALPHA-5"
+    version = "1.0.0-ALPHA-5-kotlin-1.8.20-Beta"
 
     repositories {
         mavenCentral()
