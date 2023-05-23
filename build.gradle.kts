3d28c00a09dd47c5740f81748993d0ec201ca645
Rick Clephas <rclephas@gmail.com>
2023-05-23 17:45:15 +0200

    Fix issue where KGP was loaded multiple times

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+plugins {
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.kotlin.jvm) apply false
+}
+
 buildscript {
     repositories {
         gradlePluginPortal()
