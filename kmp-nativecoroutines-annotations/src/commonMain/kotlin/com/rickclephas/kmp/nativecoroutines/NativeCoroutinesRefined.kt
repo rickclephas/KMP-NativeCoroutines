fe5e48d844e18736c52137836ad813f363223d12
Rick Clephas <rclephas@gmail.com>
2023-07-02 12:56:40 +0200

    Add @NativeCoroutinesRefined and @NativeCoroutinesRefinedState annotations

---
+++ kmp-nativecoroutines-annotations/src/commonMain/kotlin/com/rickclephas/kmp/nativecoroutines/NativeCoroutinesRefined.kt
@@ -0,0 +1,15 @@
+package com.rickclephas.kmp.nativecoroutines
+
+import kotlin.experimental.ExperimentalObjCRefinement
+import kotlin.native.HidesFromObjC
+import kotlin.native.ShouldRefineInSwift
+
+/**
+ * Identifies properties and functions that require a native [ShouldRefineInSwift] coroutines version.
+ */
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.BINARY)
+@MustBeDocumented
+@OptIn(ExperimentalObjCRefinement::class)
+@HidesFromObjC
+annotation class NativeCoroutinesRefined
