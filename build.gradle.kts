1c14647f50fb802dd924efc56a6df3bc1e5f9ec8
Rick Clephas <rclephas@gmail.com>
2023-07-07 20:10:11 +0200

    Bump version to 1.0.0-ALPHA-13

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 
 allprojects {
     group = "com.rickclephas.kmp"
-    version = "1.0.0-ALPHA-12-kotlin-1.9.0-RC"
+    version = "1.0.0-ALPHA-13"
 
     repositories {
         mavenCentral()
