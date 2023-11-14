e43ffaca963c0ecd97b5cadfb473ae4f7ab534c9
Rick Clephas <rclephas@gmail.com>
2023-11-14 15:16:39 +0100

    Add FIR diagnostics tests

--- kmp-nativecoroutines-compiler/build.gradle.kts
+++ kmp-nativecoroutines-compiler/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.utils.NativeCompilerDownloader
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     `kmp-nativecoroutines-publish`
@@ -38,6 +40,7 @@ tasks.compileKotlin.configure {
 }
 
 tasks.test {
+    systemProperty("kotlin.internal.native.test.nativeHome", NativeCompilerDownloader(project).compilerDirectory.absolutePath)
     inputs.dir("src/testData")
     useJUnitPlatform()
 }
