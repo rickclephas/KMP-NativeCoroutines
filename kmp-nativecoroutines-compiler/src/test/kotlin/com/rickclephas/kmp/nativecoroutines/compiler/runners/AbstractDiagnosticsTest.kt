94566281f81eae19f58f2ee94f4e3047b7ddd3c6
Rick Clephas <rclephas@gmail.com>
2023-11-16 16:07:09 +0100

    Fix compiler tests after 2.0.0-Beta1 update

--- kmp-nativecoroutines-compiler/src/test/kotlin/com/rickclephas/kmp/nativecoroutines/compiler/runners/AbstractDiagnosticsTest.kt
+++ kmp-nativecoroutines-compiler/src/test/kotlin/com/rickclephas/kmp/nativecoroutines/compiler/runners/AbstractDiagnosticsTest.kt
@@ -3,11 +3,13 @@ package com.rickclephas.kmp.nativecoroutines.compiler.runners
 import org.jetbrains.kotlin.platform.konan.NativePlatforms
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
 import org.jetbrains.kotlin.test.builders.classicFrontendHandlersStep
+import org.jetbrains.kotlin.test.directives.ConfigurationDirectives
 import org.jetbrains.kotlin.test.frontend.classic.ClassicFrontendFacade
 import org.jetbrains.kotlin.test.frontend.classic.handlers.ClassicDiagnosticsHandler
 import org.jetbrains.kotlin.test.frontend.classic.handlers.OldNewInferenceMetaInfoProcessor
 import org.jetbrains.kotlin.test.model.DependencyKind
 import org.jetbrains.kotlin.test.model.FrontendKinds
+import org.jetbrains.kotlin.test.services.LibraryProvider
 import org.jetbrains.kotlin.test.services.configuration.CommonEnvironmentConfigurator
 
 abstract class AbstractDiagnosticsTest: AbstractKmpNativeCoroutinesTest() {
@@ -18,12 +20,16 @@ abstract class AbstractDiagnosticsTest: AbstractKmpNativeCoroutinesTest() {
             targetPlatform = NativePlatforms.unspecifiedNativePlatform
             dependencyKind = DependencyKind.Source
         }
+        defaultDirectives {
+            +ConfigurationDirectives.WITH_STDLIB
+        }
         enableMetaInfoHandler()
         useConfigurators(::CommonEnvironmentConfigurator)
         useMetaInfoProcessors(::OldNewInferenceMetaInfoProcessor)
         facadeStep(::ClassicFrontendFacade)
         classicFrontendHandlersStep {
             useHandlers(::ClassicDiagnosticsHandler)
+            useAdditionalService(::LibraryProvider)
         }
     }
 }
