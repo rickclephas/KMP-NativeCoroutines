016ba16c2eb2a8f076008952215a0bb40b3c601b
Rick Clephas <rclephas@gmail.com>
2024-02-18 17:24:38 +0100

    Use new Key class
    https://youtrack.jetbrains.com/issue/KTIJ-27235

--- kmp-nativecoroutines-idea-plugin/src/main/kotlin/com/rickclephas/kmp/nativecoroutines/idea/gradle/KmpNativeCoroutinesModel.kt
+++ kmp-nativecoroutines-idea-plugin/src/main/kotlin/com/rickclephas/kmp/nativecoroutines/idea/gradle/KmpNativeCoroutinesModel.kt
@@ -1,6 +1,6 @@
 package com.rickclephas.kmp.nativecoroutines.idea.gradle
 
-import com.intellij.openapi.util.Key
+import com.intellij.openapi.externalSystem.model.Key
 import java.io.Serializable
 
 public interface KmpNativeCoroutinesModel: Serializable {
@@ -11,4 +11,4 @@ internal class KmpNativeCoroutinesModelImpl(
     override val exposedSeverity: String
 ): KmpNativeCoroutinesModel
 
-internal val KmpNativeCoroutinesModelKey = Key<KmpNativeCoroutinesModel>(KmpNativeCoroutinesModel::class.java.name)
+internal val KmpNativeCoroutinesModelKey = Key<KmpNativeCoroutinesModel>(KmpNativeCoroutinesModel::class.java.name, 1)
