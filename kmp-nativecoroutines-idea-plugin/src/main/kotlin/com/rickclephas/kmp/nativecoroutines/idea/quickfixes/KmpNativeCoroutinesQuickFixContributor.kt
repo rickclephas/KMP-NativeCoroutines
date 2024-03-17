7f2149dcb46911afae1106e04a3535c9c25d4edd
Rick Clephas <rclephas@gmail.com>
2024-03-17 12:45:53 +0100

    Convert REDUNDANT_OVERRIDE to INCOMPATIBLE_OVERRIDE and add INCOMPATIBLE_ACTUAL

--- kmp-nativecoroutines-idea-plugin/src/main/kotlin/com/rickclephas/kmp/nativecoroutines/idea/quickfixes/KmpNativeCoroutinesQuickFixContributor.kt
+++ kmp-nativecoroutines-idea-plugin/src/main/kotlin/com/rickclephas/kmp/nativecoroutines/idea/quickfixes/KmpNativeCoroutinesQuickFixContributor.kt
@@ -11,17 +11,22 @@ import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNati
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.IGNORED_COROUTINES_REFINED
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.IGNORED_COROUTINES_REFINED_STATE
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.IGNORED_COROUTINES_STATE
+import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INCOMPATIBLE_ACTUAL_COROUTINES
+import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INCOMPATIBLE_ACTUAL_COROUTINES_IGNORE
+import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INCOMPATIBLE_ACTUAL_COROUTINES_REFINED
+import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INCOMPATIBLE_ACTUAL_COROUTINES_REFINED_STATE
+import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INCOMPATIBLE_ACTUAL_COROUTINES_STATE
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INVALID_COROUTINES
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INVALID_COROUTINES_IGNORE
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INVALID_COROUTINES_REFINED
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INVALID_COROUTINES_REFINED_STATE
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INVALID_COROUTINES_STATE
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INVALID_COROUTINE_SCOPE
-import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.REDUNDANT_OVERRIDE_COROUTINES
-import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.REDUNDANT_OVERRIDE_COROUTINES_IGNORE
-import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.REDUNDANT_OVERRIDE_COROUTINES_REFINED
-import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.REDUNDANT_OVERRIDE_COROUTINES_REFINED_STATE
-import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.REDUNDANT_OVERRIDE_COROUTINES_STATE
+import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INCOMPATIBLE_OVERRIDE_COROUTINES
+import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INCOMPATIBLE_OVERRIDE_COROUTINES_IGNORE
+import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INCOMPATIBLE_OVERRIDE_COROUTINES_REFINED
+import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INCOMPATIBLE_OVERRIDE_COROUTINES_REFINED_STATE
+import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.INCOMPATIBLE_OVERRIDE_COROUTINES_STATE
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.REDUNDANT_PRIVATE_COROUTINES
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.REDUNDANT_PRIVATE_COROUTINES_IGNORE
 import com.rickclephas.kmp.nativecoroutines.compiler.classic.diagnostics.KmpNativeCoroutinesErrors.REDUNDANT_PRIVATE_COROUTINES_REFINED
@@ -46,21 +51,31 @@ public class KmpNativeCoroutinesQuickFixContributor: QuickFixContributor {
         )
         quickFixes.registerRemoveAnnotationFix(
             CONFLICT_COROUTINES,
+
             IGNORED_COROUTINES,
             IGNORED_COROUTINES_REFINED,
             IGNORED_COROUTINES_REFINED_STATE,
             IGNORED_COROUTINES_STATE,
+
             INVALID_COROUTINES,
             INVALID_COROUTINE_SCOPE,
             INVALID_COROUTINES_IGNORE,
             INVALID_COROUTINES_REFINED,
             INVALID_COROUTINES_REFINED_STATE,
             INVALID_COROUTINES_STATE,
-            REDUNDANT_OVERRIDE_COROUTINES,
-            REDUNDANT_OVERRIDE_COROUTINES_IGNORE,
-            REDUNDANT_OVERRIDE_COROUTINES_REFINED,
-            REDUNDANT_OVERRIDE_COROUTINES_REFINED_STATE,
-            REDUNDANT_OVERRIDE_COROUTINES_STATE,
+
+            INCOMPATIBLE_OVERRIDE_COROUTINES,
+            INCOMPATIBLE_OVERRIDE_COROUTINES_IGNORE,
+            INCOMPATIBLE_OVERRIDE_COROUTINES_REFINED,
+            INCOMPATIBLE_OVERRIDE_COROUTINES_REFINED_STATE,
+            INCOMPATIBLE_OVERRIDE_COROUTINES_STATE,
+
+            INCOMPATIBLE_ACTUAL_COROUTINES,
+            INCOMPATIBLE_ACTUAL_COROUTINES_IGNORE,
+            INCOMPATIBLE_ACTUAL_COROUTINES_REFINED,
+            INCOMPATIBLE_ACTUAL_COROUTINES_REFINED_STATE,
+            INCOMPATIBLE_ACTUAL_COROUTINES_STATE,
+
             REDUNDANT_PRIVATE_COROUTINES,
             REDUNDANT_PRIVATE_COROUTINES_IGNORE,
             REDUNDANT_PRIVATE_COROUTINES_REFINED,
