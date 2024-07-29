bb93d69a9024dea02882f1293709b9ecf95b9fa6
Rick Clephas <rclephas@gmail.com>
2024-07-29 13:15:33 +0200

    Create asFirExpression util function

--- kmp-nativecoroutines-compiler/src/main/kotlin/com/rickclephas/kmp/nativecoroutines/compiler/fir/codegen/TodoCall.kt
+++ kmp-nativecoroutines-compiler/src/main/kotlin/com/rickclephas/kmp/nativecoroutines/compiler/fir/codegen/TodoCall.kt
@@ -1,17 +1,16 @@
 package com.rickclephas.kmp.nativecoroutines.compiler.fir.codegen
 
+import com.rickclephas.kmp.nativecoroutines.compiler.fir.utils.asFirExpression
 import com.rickclephas.kmp.nativecoroutines.compiler.utils.CallableIds
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.expressions.FirFunctionCall
 import org.jetbrains.kotlin.fir.expressions.buildResolvedArgumentList
 import org.jetbrains.kotlin.fir.expressions.builder.buildFunctionCall
-import org.jetbrains.kotlin.fir.expressions.builder.buildLiteralExpression
 import org.jetbrains.kotlin.fir.references.builder.buildResolvedNamedReference
 import org.jetbrains.kotlin.fir.resolve.providers.symbolProvider
 import org.jetbrains.kotlin.fir.symbols.SymbolInternals
 import org.jetbrains.kotlin.fir.types.constructClassLikeType
 import org.jetbrains.kotlin.name.StandardClassIds
-import org.jetbrains.kotlin.types.ConstantValueKind
 
 internal fun FirSession.buildTodoCall(reason: String): FirFunctionCall = buildFunctionCall {
     val callableId = CallableIds.todo
@@ -20,8 +19,7 @@ internal fun FirSession.buildTodoCall(reason: String): FirFunctionCall = buildFu
     coneTypeOrNull = StandardClassIds.Nothing.constructClassLikeType()
     @OptIn(SymbolInternals::class)
     val reasonParameter = todoSymbol.valueParameterSymbols.first().fir
-    val reasonExpression = buildLiteralExpression(null, ConstantValueKind.String, reason, setType = true)
-    argumentList = buildResolvedArgumentList(null, linkedMapOf(reasonExpression to reasonParameter))
+    argumentList = buildResolvedArgumentList(null, linkedMapOf(reason.asFirExpression() to reasonParameter))
     calleeReference = buildResolvedNamedReference {
         name = todoSymbol.name
         resolvedSymbol = todoSymbol
