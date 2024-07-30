1fd6920e5d28990a94cdcb8f7db4062d19e807e0
Rick Clephas <rclephas@gmail.com>
2024-07-30 21:49:34 +0200

    Add error message for missing KSP plugin

--- kmp-nativecoroutines-gradle-plugin/src/main/kotlin/com/rickclephas/kmp/nativecoroutines/gradle/KmpNativeCoroutinesPlugin.kt
+++ kmp-nativecoroutines-gradle-plugin/src/main/kotlin/com/rickclephas/kmp/nativecoroutines/gradle/KmpNativeCoroutinesPlugin.kt
@@ -46,6 +46,9 @@ public class KmpNativeCoroutinesPlugin: KotlinCompilerPluginSupportPlugin {
     override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
         val project = kotlinCompilation.target.project
         val extension = project.extensions.getByType(KmpNativeCoroutinesExtension::class.java)
+        if (!extension.k2Mode && !project.plugins.hasPlugin(kspPluginId)) {
+            project.logger.error("KMP-NativeCoroutines plugin applied without KSP plugin")
+        }
         return project.provider {
             buildList {
                 add(SubpluginOption("exposedSeverity", extension.exposedSeverity.name))
