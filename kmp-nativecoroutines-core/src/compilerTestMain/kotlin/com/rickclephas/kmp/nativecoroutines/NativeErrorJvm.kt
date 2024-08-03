4debf8f7cbf5c26a73cffe8b0e3c7fcfb44efc16
Rick Clephas <rclephas@gmail.com>
2024-08-03 14:21:31 +0200

    Update NativeError implementations for JVM

--- kmp-nativecoroutines-core/src/compilerTestMain/kotlin/com/rickclephas/kmp/nativecoroutines/NativeErrorJvm.kt
+++
@@ -1,5 +0,0 @@
-package com.rickclephas.kmp.nativecoroutines
-
-public actual typealias NativeError = Throwable
-
-internal actual fun Throwable.asNativeError(): Throwable = this
--- kmp-nativecoroutines-core/src/compilerTestTest/kotlin/com/rickclephas/kmp/nativecoroutines/NativeErrorTestUtils.jvm.kt
+++ kmp-nativecoroutines-core/src/compilerTestTest/kotlin/com/rickclephas/kmp/nativecoroutines/NativeErrorTestUtils.jvm.kt
@@ -1,3 +1,6 @@
 package com.rickclephas.kmp.nativecoroutines
 
 internal actual val NativeError.kotlinCause: Throwable? get() = this
+
+@Suppress("TestFunctionName")
+internal actual fun RandomNativeError(): NativeError = RandomException()
