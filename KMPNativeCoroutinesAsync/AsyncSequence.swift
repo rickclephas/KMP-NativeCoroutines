6395ebe85b60ea824c5a7fe532c1b000c042dac1
Stijn <stijn.willems@mediahuis.be>
2025-02-28 12:18:10 +0100

    Add buffer policy as an option and some docs explaining the difference

--- KMPNativeCoroutinesAsync/AsyncSequence.swift
+++ KMPNativeCoroutinesAsync/AsyncSequence.swift
@@ -10,10 +10,17 @@ import Foundation
 import KMPNativeCoroutinesCore
 
 /// Converts a NativeFlow to an ``AsyncThrowingStream``
+/// - Parameters:
+///   - nativeFlow: the coroutine flow that is translated from kotlin to swift exposes closured. This is a typealias
+///   - bufferingPolicy: The default policy in Kotlin for flows is to consume directly and suspend until another value arrived.
+///   `AsyncThrowingStream` will buffer unbounded  by default if the consumer cannot directly consume the value.
+///   Change it to `.bufferingNewest(1)` if you want to match the Kotlin behaviour
+/// - Returns: An ``AsyncThrowingStream``  that can be consumed wit `for try await in ...`.
 public func asyncSequence<Output, Failure: Error, Unit>(
-  for nativeFlow: @escaping NativeFlow<Output, Failure, Unit>
+  for nativeFlow: @escaping NativeFlow<Output, Failure, Unit>,
+  bufferingPolicy: AsyncThrowingStream<Output, Error>.Continuation.BufferingPolicy = .unbounded
 ) -> AsyncThrowingStream<Output, Error> {
-  AsyncThrowingStream { continuation in
+    AsyncThrowingStream(bufferingPolicy: bufferingPolicy) { continuation in
     let lock = NSRecursiveLock()
     
     let callbacks = FlowCallbacks<Output, Failure, Unit>(
