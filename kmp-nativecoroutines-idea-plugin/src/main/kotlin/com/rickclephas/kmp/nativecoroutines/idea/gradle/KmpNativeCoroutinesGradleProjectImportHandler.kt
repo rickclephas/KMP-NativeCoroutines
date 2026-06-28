98fea741684b369e2fb2160a1f140bf02db2bf4e
Rick Clephas <rclephas@gmail.com>
2026-06-28 16:07:18 +0200

    Update KmpNativeCoroutinesGradleProjectImportHandler implementation
    https://github.com/JetBrains/intellij-community/commit/a92c2dc6a86741b793601d759f3d6cb7436345dc

--- kmp-nativecoroutines-idea-plugin/src/main/kotlin/com/rickclephas/kmp/nativecoroutines/idea/gradle/KmpNativeCoroutinesGradleProjectImportHandler.kt
+++ kmp-nativecoroutines-idea-plugin/src/main/kotlin/com/rickclephas/kmp/nativecoroutines/idea/gradle/KmpNativeCoroutinesGradleProjectImportHandler.kt
@@ -1,40 +1,13 @@
 package com.rickclephas.kmp.nativecoroutines.idea.gradle
 
 import com.intellij.openapi.application.PathManager
-import com.intellij.openapi.externalSystem.model.Key
 import com.rickclephas.kmp.nativecoroutines.compiler.KmpNativeCoroutinesCommandLineProcessor
-import com.rickclephas.kmp.nativecoroutines.compiler.config.*
-import org.jetbrains.kotlin.idea.compilerPlugin.CompilerPluginSetup.PluginOption
-import org.jetbrains.kotlin.idea.gradleJava.compilerPlugin.AbstractCompilerPluginGradleImportHandler
+import org.jetbrains.kotlin.idea.gradleJava.compilerPlugin.AbstractGradleImportHandler
+import java.nio.file.Path
 
-public class KmpNativeCoroutinesGradleProjectImportHandler: AbstractCompilerPluginGradleImportHandler<KmpNativeCoroutinesModel>() {
-
-    override val compilerPluginId: String = "com.rickclephas.kmp.nativecoroutines"
-    override val modelKey: Key<KmpNativeCoroutinesModel> = KmpNativeCoroutinesModelKey
-    override val pluginJarFileFromIdea: String
-        get() = PathManager.getJarPathForClass(KmpNativeCoroutinesCommandLineProcessor::class.java)!!
-    override val pluginName: String = "KMP-NativeCoroutines"
-
-    private fun MutableList<PluginOption>.add(option: ConfigOption<*>, value: String) =
-        add(PluginOption(option.optionName, value))
-
-    private fun MutableList<PluginOption>.addAll(option: ConfigListOption<*>, values: List<String>) {
-        for (value in values) {
-            add(PluginOption(option.optionName, value))
-        }
-    }
-
-    override fun getOptions(
-        model: KmpNativeCoroutinesModel
-    ): List<PluginOption> = buildList {
-        add(SUFFIX, model.suffix)
-        model.flowValueSuffix?.let { add(FLOW_VALUE_SUFFIX, it) }
-        model.flowReplayCacheSuffix?.let { add(FLOW_REPLAY_CACHE_SUFFIX, it) }
-        add(STATE_SUFFIX, model.stateSuffix)
-        model.stateFlowSuffix?.let { add(STATE_FLOW_SUFFIX, it) }
-        add(EXPOSED_SEVERITY, model.exposedSeverity)
-        addAll(GENERATED_SOURCE_DIR, model.generatedSourceDirs)
-        val swiftExport = model.swiftExportVersion.takeIf { model.swiftExport } ?: 0
-        add(SWIFT_EXPORT, swiftExport.toString())
-    }
+public class KmpNativeCoroutinesGradleProjectImportHandler: AbstractGradleImportHandler() {
+    override val pluginJarsToReplaceRegex: List<Regex>
+        get() = listOf("kmp-nativecoroutines-compiler-.*\\.jar".toRegex())
+    override val replacementJarFromPluginBundle: Path
+        get() = PathManager.getJarForClass(KmpNativeCoroutinesCommandLineProcessor::class.java)!!
 }
